f8cf378d099369bdce08479980b60f00134a16b6
parmesh10 <parmesh.aavula@gmail.com>
2026-08-20 02:04:56 +0530

    Added questions-2 added

--- TRDinfo/settings.py
+++ TRDinfo/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
     #"10.54.19.27",
 #]
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS = ["trdinfo.onrender.com",
     
 ]
 # Application definition
@@ -130,7 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+
+STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
